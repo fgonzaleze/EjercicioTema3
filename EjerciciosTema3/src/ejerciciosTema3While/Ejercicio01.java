5d3800b52474925ccd2aea54ab78e4082310cb40
jgonzalez <jgonzalez@106-07.info2>
2022-10-25 12:45:37 +0200

    25/10/2022 ejercicios 1 y 2

--- EjerciciosTema3/src/ejerciciosTema3While/Ejercicio01.java
+++ EjerciciosTema3/src/ejerciciosTema3While/Ejercicio01.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
 public class Ejercicio01 {
 
 	public static void main(String[] args) {
+
 		int suma = 0;
 		int num = 0;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Introduce un número entero positivo para sumar, y uno negativo para acabar la suma");
+		System.out.println("Introduce números enteros para contar");
 		num=sc.nextInt();
 		while (num>=0) {
 			suma += num;
 			System.out.println(suma);
 			num=sc.nextInt();
 		}
-		System.out.println("Suma: " + suma);
+		System.out.println("Suma: " + suma );
 		sc.close();
+
 	}
 
 }
---
+++ EjerciciosTema3/src/ejerciciosTema3While/Ejercicio03.java
@@ -0,0 +1,10 @@
+package ejerciciosTema3While;
+
+public class Ejercicio03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
