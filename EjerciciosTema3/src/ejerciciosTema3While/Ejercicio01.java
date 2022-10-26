f30e472e60829b3b71dc20342c30c6f4a4e93d38
jgonzalez <jgonzalez@106-07.info2>
2022-10-26 13:02:53 +0200

    26/10

--- EjerciciosTema3/src/ejerciciosTema3While/Ejercicio01.java
+++ EjerciciosTema3/src/ejerciciosTema3While/Ejercicio01.java
@@ -6,12 +6,12 @@ public class Ejercicio01 {
 
 	public static void main(String[] args) {
 
-		int suma = 0;
-		int num = 0;
+		int suma = 0;														// Establecemos la suma y el int como 0 para que a la hora de  
+		int num = 0;														// empezar a sumar partamos de un valor neutro
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Introduce números enteros para contar");
-		num=sc.nextInt();
-		while (num>=0) {
+		System.out.println("Introduce números enteros para contar");		// Pedimos con el escáner los números enteros para ir llevando la cuenta
+		num=sc.nextInt();													// de la cantidad de números que vamos pidiendo
+		while (num>=0) {													// Creamos un bucle 
 			suma += num;
 			System.out.println(suma);
 			num=sc.nextInt();
